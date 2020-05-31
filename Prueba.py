5f10ed01967da217f488e9329b8e84a3cc798d83
cocon99 <santaaelena23@gmail.com>
2020-05-31 13:34:52 -0600

    Agregamos funciones básicas.

--- Prueba.py
+++ Prueba.py
@@ -2,6 +2,21 @@ import tkinter as tk
 import cv2
 import numpy as np
 
+comandos = []
+
+def ActualizarDisplay(Boton):
+    global comandos
+    print(f"comando es {Boton}")
+    if(Boton == 'AC'):
+        comandos = []
+    elif(Boton == 'DEL'):
+        if(len(comandos) > 0):
+            comandos.pop()
+    else:
+        comandos.append(Boton)
+    print(comandos)
+    CajaTexto.set(comandos)
+
 # Dibujar
 def DibujarBoton(pantalla, nombre, _x, _y):
     TeclaFor = tk.Button(pantalla)
@@ -14,9 +29,11 @@ def DibujarBoton(pantalla, nombre, _x, _y):
     TeclaFor["bg"] = ("#242F40")
     TeclaFor["fg"] = ("#F6E8EA")
     TeclaFor.place(x=_x, y=_y)
+    TeclaFor['command'] = lambda:ActualizarDisplay(nombre)
 
 # Principal
 if __name__ == "__main__":
+    global CajaTexto
     print("Codigo principal")
 
     ventana = tk.Tk()
@@ -24,9 +41,9 @@ if __name__ == "__main__":
     ventana.geometry("290x320")
     ventana.configure(background="#437F97")
 
-    input_text = tk.StringVar()
-    entrada=tk.Entry(ventana,textvariable=input_text,bg="#F6E8EA")
-    entrada.pack(fill=tk.X,padx=10,pady=15,ipadx=5,ipady=4)
+    CajaTexto = tk.StringVar()
+    entrada = tk.Entry(ventana, textvariable= CajaTexto, bg="#F6E8EA", fg="#242F40")
+    entrada.pack(fill=tk.X,padx=10, pady=15, ipadx=5, ipady=4)
 
     tecla = ['AC', 'DEL', '=', 'P', 'Q','R','S','~','v','^','->','<->','+','(',')','IZQUIERDA','DERECHA',]
     X_tecla = [10,80,150,10,80,150,220,10,80,150,220,10,80,150,220,10,150]
