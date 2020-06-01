da3a8e24fa8ddd5202c3a324ea674b80600de1e4
cocon99 <santaaelena23@gmail.com>
2020-06-01 11:10:57 -0600

    AVANZANDO

--- Prueba.py
+++ Prueba.py
@@ -4,18 +4,65 @@ import numpy as np
 
 comandos = []
 
+def DibujarRespuesta(comandosActual):
+    print(f"empezar operacion {comandosActual}")
+    VentanaRespuesta=tk.Toplevel()
+    VentanaRespuesta.geometry("400x400")
+    VentanaRespuesta.configure(background="#437F97")
+    Entradas = ""
+    Entradas = []
+    for comandoTemporal in comandosActual:
+        SeEncuentra = False
+        try:
+            if(Entradas.index(comandoTemporal) >= 0 ):
+                SeEncuentra = True
+        except:
+            print("No es lista")
+        if(not SeEncuentra):
+            if(comandoTemporal == 'P' or comandoTemporal == 'Q' or comandoTemporal == 'R' or comandoTemporal == 'S'):
+                Entradas.append(comandoTemporal)
+    print(f"Las estrados son: {Entradas}")
+    for x in range(len(comandos)):
+        CuadroTemporal = tk.Label(VentanaRespuesta)
+        CuadroTemporal["text"] = comandos[x]
+        CuadroTemporal["width"] = 10
+        CuadroTemporal["height"] = 1
+        CuadroTemporal.grid(row=0, column=x)
+
+    for y in range(2 ** len(Entradas)):
+        EntradaTable = []
+        Y_Temporal = y
+        for x in range(len(Entradas)):
+            EntradaTable.append(Y_Temporal % 2)
+            Y_Temporal = Y_Temporal // 2
+            CuadroTemporal = tk.Label(VentanaRespuesta)
+            CuadroTemporal["text"] = EntradaTable[x]
+            CuadroTemporal["width"] = 10
+            CuadroTemporal["height"] = 1
+            CuadroTemporal.grid(row=y+1, column=x)
+
+
 def ActualizarDisplay(Boton):
     global comandos
     print(f"comando es {Boton}")
     if(Boton == 'AC'):
         comandos = []
+        print("Limpiando comandos ")
     elif(Boton == 'DEL'):
         if(len(comandos) > 0):
             comandos.pop()
+            print("Elimiando ultimo")
+        else:
+            print("No hay que eliminar")
+    elif(Boton == '='):
+        if(len(comandos) > 0):
+            print("Monstando respuesta")
+            DibujarRespuesta(comandos)
     else:
         comandos.append(Boton)
-    print(comandos)
+        print(f"Imprimiendo comandos: {comandos}")
     CajaTexto.set(comandos)
+
 
 # Dibujar
 def DibujarBoton(pantalla, nombre, _x, _y):
@@ -53,3 +100,14 @@ if __name__ == "__main__":
         DibujarBoton(ventana,tecla[TeclaActual], X_tecla[TeclaActual], Y_tecla[TeclaActual])
 
     ventana.mainloop()
+
+# simple 2d table
+
+# from tkinter import *
+#
+# for i in range(5):
+#        for j in range(4):
+#            l = Label(text='%d.%d' % (i, j), relief=RIDGE)
+#            l.grid(row=i, column=j, sticky=NSEW)
+#
+# mainloop()
